61d66c773648735e125d1584a500845c3e44711a
zhenyaracheva <zhenia.racheva@gmail.com>
2015-12-13 21:48:04 +0200

    Removed # from url

--- public/app/app.js
+++ public/app/app.js
@@ -3,9 +3,12 @@
 
   angular.module('BullsAndCows.controllers', []);
   angular.module('BullsAndCows.services', []);
-  angular.module('BullsAndCows', ['ngRoute', 'BullsAndCows.controllers','BullsAndCows.services']);
+  angular.module('BullsAndCows', ['ngRoute', 'BullsAndCows.controllers', 'BullsAndCows.services']);
 
-  angular.module('BullsAndCows').config(['$routeProvider', function ($routeProvider) {
+  angular.module('BullsAndCows').config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+
+    $locationProvider.html5Mode(true);
+
     $routeProvider
       .when('/', {
         templateUrl: 'app/views/home.html',
@@ -17,12 +20,12 @@
         controller: 'CreateGameController',
         controllerAs: 'vm'
       })
-      .when('/users/register',{
+      .when('/users/register', {
         templateUrl: 'app/views/register-user.html',
         controller: 'RegisterUserController',
         controllerAs: 'vm'
       })
-      .when('/users/login',{
+      .when('/users/login', {
         templateUrl: 'app/views/login-user.html',
         controller: 'LoginUserController',
         controllerAs: 'vm'
